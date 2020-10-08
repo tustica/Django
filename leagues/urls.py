31c7679738329d49398ac831956c395fd410733a
Devon <=>
2020-10-08 09:52:11 -0700

    url fix

--- leagues/urls.py
+++ leagues/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name="index"),
-	path('', views.make_data, name="make_data"),
+	path('initialize', views.make_data, name="make_data"),
 ]
