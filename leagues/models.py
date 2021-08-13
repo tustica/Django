3735d6605163cc1cce4344d1a905eedb32aff567
tustica <tim.ustica@gmail.com>
2021-08-13 16:14:57 -0500

    First commit

--- leagues/models.py
+++ leagues/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 class League(models.Model):
 	name = models.CharField(max_length=50)
--- leagues/views.py
+++ leagues/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render, redirect
 from .models import League, Team, Player
-
 from . import team_maker
+import leagues
+from django.db.models import Count
 
 def index(request):
 	context = {
 		"leagues": League.objects.all(),
+		"baseball_leagues": League.objects.filter(sport="Baseball"),
 		"teams": Team.objects.all(),
 		"players": Player.objects.all(),
+		"dallas_teams": Team.objects.filter(location = "Dallas"),
+		"penguins" : Team.objects.get(team_name = "Penguins"),
+		"football_leagues": League.objects.filter(sport = "Football"),
+		"count_of_players": Team.objects.annotate(Count('all_players')),
+		"count_of_teams": Player.objects.annotate(countnum = Count('all_teams'))
+		
 	}
 	return render(request, "leagues/index.html", context)
 
